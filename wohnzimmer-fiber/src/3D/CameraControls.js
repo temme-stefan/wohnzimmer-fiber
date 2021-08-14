65b632f79739c62480772949c0289d5150adddcc
Stefan Temme <temme@objectcode.de>
2021-08-14 23:01:41 +0200

    room

---
+++ wohnzimmer-fiber/src/3D/CameraControls.js
@@ -0,0 +1,19 @@
+import {extend, useFrame, useThree} from "@react-three/fiber";
+import {useRef} from "react";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
+
+extend({OrbitControls});
+
+export default function CameraControls() {
+    // Get a reference to the Three.js Camera, and the canvas html element.
+    // We need these to setup the OrbitControls component.
+    // https://threejs.org/docs/#examples/en/controls/OrbitControls
+    const {
+        camera,
+        gl: {domElement},
+    } = useThree();
+    // Ref to the controls, so that we can update them on every frame using useFrame
+    const controls = useRef();
+    useFrame((state) => controls.current.update());
+    return <orbitControls ref={controls} args={[camera, domElement]} target={camera.target}/>;
+};
--- wohnzimmer-fiber/src/3D/STAGE.js
+++ wohnzimmer-fiber/src/3D/STAGE.js
@@ -1,25 +1,8 @@
-import {Canvas, extend, useFrame,useThree} from "@react-three/fiber";
-import {useRef} from "react";
+import {Canvas, extend} from "@react-three/fiber";
 import Room from "./Room.js";
+import CameraControls from "./CameraControls.js";
 
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
-import {Vector3} from "three";
 
-extend({OrbitControls});
-
-const CameraControls = () => {
-    // Get a reference to the Three.js Camera, and the canvas html element.
-    // We need these to setup the OrbitControls component.
-    // https://threejs.org/docs/#examples/en/controls/OrbitControls
-    const {
-        camera,
-        gl: { domElement },
-    } = useThree();
-    // Ref to the controls, so that we can update them on every frame using useFrame
-    const controls = useRef();
-    useFrame((state) => controls.current.update());
-    return <orbitControls ref={controls} args={[camera, domElement]} target={camera.target} />;
-};
 
 export default function STAGE(){
 
